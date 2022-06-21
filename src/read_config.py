981f7afb4e33d45824fdf453fe2c9aae7385e7c4
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-06-21 18:20:21 +0100

    Config File: Pseudocode brainstorm

---
+++ src/read_config.py
@@ -0,0 +1,15 @@
+# TODO:
+#     read_config_file(foo)
+#         Open config file
+#         Read each line and ignore comments
+#         Split in '=' sign
+#         Store values in variables
+#         Close config file
+#         return foo
+#
+#     get_foo()
+#         Call read_config_file(foo)
+#         return foo
+#
+#
+#     Remind to google the best way for doing this in Python
