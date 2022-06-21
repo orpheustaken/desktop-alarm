becf7b0268802007134df220d4c8a5710c964e58
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-06-21 18:27:33 +0100

    Config File: Pseudocode brainstorm

--- src/read_config.py
+++ src/read_config.py
@@ -2,13 +2,15 @@
 #     read_config_file(foo)
 #         Open config file
 #         Read each line and ignore comments
+#             Looking for foo
 #         Split in '=' sign
-#         Store values in variables
+#         Store foo value in a variable
 #         Close config file
 #         return foo
 #
 #     get_foo()
 #         Call read_config_file(foo)
+#         Store foo value in a variable
 #         return foo
 #
 #
