6f95437f802fd0e4627ed357901ec79af4e9dcf3
Matheus Vinicius <matheusvinicius7000@gmail.com>
2022-06-26 22:22:18 +0100

    System Time: Address performance issue while looping

--- src/time_sys.py
+++ src/time_sys.py
@@ -1,10 +1,14 @@
 import datetime
+import time
 
 
-def check_system_time_loop(time):
+def check_system_time_loop(time_input):
     while 1:
         system_time = datetime.datetime.now()
         system_time_formatted = system_time.strftime("%H:%M")
 
-        if system_time_formatted == time:
+        if system_time_formatted == time_input:
             break
+
+        # To address performance issues
+        time.sleep(40)
