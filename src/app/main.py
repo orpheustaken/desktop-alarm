25fff9a12d3ecc7c237fdf405ee02134f04a0dd7
Matheus Fernandes <matheusvinicius7000@gmail.com>
2024-08-08 18:14:38 -0300

    .gitignore: Add .idea/ folder

---
+++ src/app/main.py
@@ -0,0 +1,23 @@
+from src import audio, equation, message, time_sys, user_input
+
+class Main:
+
+    message.print_title()
+
+    time_input = user_input.handle_user_input()
+
+    message.print_execution_time(time_input)
+
+    # Wait until time defined by user to proceed
+    time_sys.check_system_time_loop(time_input)
+
+    audio.play_audio()
+
+    message.print_random_quote()
+
+    # Equations to solve for then be able to stop the sound and exit
+    equation.enter_equation_loop()
+
+    audio.stop_audio()
+
+    exit(0)
